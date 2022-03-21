7cfb94cbe1437d0ae83b91a4b6ddabe64f708b39
Bedadeepa <sahoodeepa97@gmail.com>
2022-03-21 17:18:48 +0530

    First Commit

---
+++ QMS/App_Start/RouteConfig.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace QMS
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+           
+            routes.MapRoute(
+           name: "Login",
+           url: "Login",
+           defaults: new { controller = "Registration", action = "Login", id = UrlParameter.Optional }
+           );
+            routes.MapRoute(
+           name: "Logout",
+           url: "Logout",
+           defaults: new { controller = "Registration", action = "Logout", id = UrlParameter.Optional }
+           );
+            
+
+            routes.MapRoute(
+          name: "Dashboard",
+          url: "Dashboard",
+          defaults: new { controller = "Admin", action = "UserPanel", id = UrlParameter.Optional }
+          );
+            routes.MapRoute(
+               name: "Default",
+               url: "{controller}/{action}/{id}",
+               defaults: new { controller = "Registration", action = "Login", id = UrlParameter.Optional }
+           );
+        }
+    }
+}
